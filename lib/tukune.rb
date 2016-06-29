9f882f9a5eb38119602213f7127e8c8e8abcd10e
gin0606 <kkgn06@gmail.com>
2016-06-29 16:34:12 +0900

    Update lib/tukune.rb

--- lib/tukune.rb
+++ lib/tukune.rb
@@ -1,7 +1,7 @@
-require "tukune/cli"
-require "tukune/client"
-require "tukune/git"
-require "tukune/version"
+require 'tukune/cli'
+require 'tukune/client'
+require 'tukune/git'
+require 'tukune/version'
 
 module Tukune
 end
