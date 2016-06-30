9a7ae7b5a64e3a637e40f35720f681afd75d7fd6
gin0606 <kkgn06@gmail.com>
2016-06-30 16:49:05 +0900

    Correct rubocop rule

--- lib/tukune/configuration/travis_ci.rb
+++ lib/tukune/configuration/travis_ci.rb
@@ -1,18 +1,18 @@
-require "tukune/configuration"
+require 'tukune/configuration'
 
 module Tukune
   class Configuration
     class TravisCI < Default
       def repository_name
-        ENV["TRAVIS_REPO_SLUG"]
+        ENV['TRAVIS_REPO_SLUG']
       end
 
       def current_branch
-        ENV["TRAVIS_BRANCH"]
+        ENV['TRAVIS_BRANCH']
       end
 
       def pull_request?
-        ENV["TRAVIS_PULL_REQUEST"] != "false"
+        ENV['TRAVIS_PULL_REQUEST'] != 'false'
       end
     end
   end
--- lib/tukune/git/commit.rb
+++ lib/tukune/git/commit.rb
@@ -23,29 +23,11 @@ module Tukune
       end
 
       def commit(message)
-        changed_blobs = blobs.map do |file_path, sha|
-          {
-            path: file_path,
-            mode: "100644",
-            type: "blob",
-            sha: sha
-          }
-        end
-
-        if delete_files.empty?
-          tree = client.create_tree(@repository_name, changed_blobs, base_tree: current_tree[:sha])
-        else
-          # FIXME: 編集したファイルが正しくcommitされない
-          changed_files = blobs.map { |path, _| path }
-          current_blobs = current_tree[:tree].map { |e| e.to_h }
-
-          trees = current_blobs
-          trees = trees.delete_if { |blob| delete_files.include?(blob[:path]) }
-          trees = trees.delete_if { |blob| changed_files.include?(blob[:path]) }
-          trees += changed_blobs
-
-          tree = client.create_tree(@repository_name, trees)
-        end
+        tree =  if delete_files.empty?
+                  create_tree(changed_blobs)
+                else
+                  create_tree_with_delete_files(changed_blobs)
+                end
 
         commits << client.create_commit(@repository_name, message, tree[:sha], current_branch[:object][:sha])
       end
@@ -62,6 +44,24 @@ module Tukune
       end
 
       private
+
+      def create_tree(changed_blobs)
+        client.create_tree(@repository_name, changed_blobs, base_tree: current_tree[:sha])
+      end
+
+      # FIXME: can not commit correctly if exist delete_files
+      def create_tree_with_delete_files(changed_blobs)
+        changed_files = blobs.map { |path, _| path }
+
+        # trees = current_tree[:tree].map(&:to_h)
+        # trees = current_tree[:tree].map(&:to_h).delete_if { |blob| delete_files.include?(blob[:path]) }
+        trees = current_tree[:tree].map(&:to_h)
+                                   .delete_if { |blob| delete_files.include?(blob[:path]) }
+                                   .delete_if { |blob| changed_files.include?(blob[:path]) }
+        trees += changed_blobs
+
+        client.create_tree(@repository_name, trees)
+      end
 
       def current_tree
         commit = client.commit(@repository_name, current_branch[:object][:sha])
@@ -87,6 +87,17 @@ module Tukune
         {}
       end
       memoize :blobs
+
+      def changed_blobs
+        blobs.map do |file_path, sha|
+          {
+            path: file_path,
+            mode: '100644',
+            type: 'blob',
+            sha: sha
+          }
+        end
+      end
 
       def delete_files
         []
